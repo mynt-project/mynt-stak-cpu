beb495b7983e4d7e7f632b6e53782f56ed14b17e
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-16 23:33:31 +0000

    core init

---
+++ _core/CMakeFiles/_core.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_core.dir/crypto/c_keccak.c.o"
+  "CMakeFiles/_core.dir/crypto/c_skein.c.o"
+  "CMakeFiles/_core.dir/crypto/c_jh.c.o"
+  "CMakeFiles/_core.dir/crypto/soft_aes.c.o"
+  "CMakeFiles/_core.dir/crypto/c_blake256.c.o"
+  "CMakeFiles/_core.dir/crypto/c_groestl.c.o"
+  "CMakeFiles/_core.dir/crypto/cryptonight_common.cpp.o"
+  "CMakeFiles/_core.dir/jconf.cpp.o"
+  "CMakeFiles/_core.dir/executor.cpp.o"
+  "CMakeFiles/_core.dir/console.cpp.o"
+  "CMakeFiles/_core.dir/jpsock.cpp.o"
+  "CMakeFiles/_core.dir/minethd.cpp.o"
+  "CMakeFiles/_core.dir/cli-miner.cpp.o"
+  "CMakeFiles/_core.dir/httpd.cpp.o"
+  "bin/_core.pdb"
+  "bin/_core"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
