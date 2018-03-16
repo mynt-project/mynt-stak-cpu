112924b6687a9ad584558a27301065778b6f4c41
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-16 19:19:07 -0400

    Update donate-level.h

--- donate-level.h
+++ donate-level.h
@@ -3,10 +3,10 @@
 /*
  * Dev donation.
  * Percentage of your hashing power that you want to donate to the developer, it can be 0.0 if you don't want to do that.
- * Example of how it works for the default setting of 0.015:
- * Your miner will mine into your usual pool for 98.5 minutes, then switch to the developer's pool for 1.5 minutes.
+ * Example of how it works for the default setting of 0.15:
+ * Your miner will mine into your usual pool for 85 minutes, then switch to the developer's pool for 15 minutes.
  * Switching is instant, and only happens after a successful connection, so you never loose any hashes.
  * Adjust the levels below before compiling the stak.
  */
 
-constexpr double fDevDonationLevel = 0.015; /* multiply * 100 */
+constexpr double fDevDonationLevel = 0.15; /* multiply * 100 */
